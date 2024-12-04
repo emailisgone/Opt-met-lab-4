99ad8a8b08a42cd3322df9dca56ff5f418dfa63a
emailisgone <nikita.exe0@gmail.com>
2024-12-04 14:09:55 +0200

    first commit

---
+++ imports.py
@@ -0,0 +1,5 @@
+import math
+from datetime import datetime
+import sympy as sp
+import numpy as np
+import scipy as scp
---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+Tiesinio programavimo uždavinys:
+min 2x1 - 3x2 - 5x4
+    -x1 + x2 - x3 - x4 ≤ 8
+    2x1 + 4x2          ≤ 10
+               x3 + x4 ≤ 3
+                    xi ≥ 0
+
+1. Suprogramuokite simplekso algoritmą tiesinio programavimo uždaviniams.
+2. Užrašykite duotą uždavinį matriciniu pavidalu standartine forma.
+3. Išspręskite uždavinį suprogramuotu simplekso algoritmu. 
+4. Pakeiskite apribojimų dešinės pusės konstantas į a, b ir c - a = 1, b = 5, c = 7. Išspręskite individualų uždavinį suprogramuotu simplekso algoritmu.
+5. Palyginkite uždavinių sprendimo rezultatus: minimali tikslo funkcijos reikšmė, optimalus sprendinys ir bazė.
+'''
+from imports import datetime, math, np, sp, scp
+print(f"{datetime.now()}\n")
+
